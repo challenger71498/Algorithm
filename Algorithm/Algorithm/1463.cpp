d91520da5a8568958b69c2c875e5274d8021321c
parang714 <challenger71498@gmail.com>
2020-01-24 13:16:56 +0900

    Add BOJ-9095

--- Algorithm/Algorithm/1463.cpp
+++ Algorithm/Algorithm/1463.cpp
@@ -1,60 +1,60 @@
-#include <iostream>
-#include <algorithm>
-
-using namespace std;
-
-int arr[1'000'001];
-
-// top-down (recursive)
-int recursive(int n) {
-	if (n == 1) {
-		return 0;
-	}
-	else if (n % 3 == 0 && n % 2 == 0) {
-		arr[n] = 1 + min({ recursive(n / 3), recursive(n / 2), recursive(n - 1) });
-		return arr[n];
-	}
-	else if (n % 3 == 0) {
-		arr[n] = 1 + min(recursive(n / 3), recursive(n - 1));
-		return arr[n];
-	}
-	else if (n % 2 == 0) {
-		arr[n] = 1 + min(recursive(n / 2), recursive(n - 1));
-		return arr[n];
-	}
-	else {
-		arr[n] = 1 + recursive(n - 1);
-		return arr[n];
-	}
-}
-
-int main() {
-
-	arr[1] = 0;
-
-	int n;
-
-	cin >> n;
-
-	// top-down (recursive)
-	//cout << recursive(n) << '\n' << endl;
-
-	// bottom-up (for-loop)
-	for (int i = 2; i <= n; ++i) {
-
-		if (i % 2 == 0 && i % 3 == 0) {
-			arr[i] = 1 + min({ arr[i / 2], arr[i / 3], arr[i - 1] });
-		}
-		else if (i % 2 == 0) {
-			arr[i] = 1 + min(arr[i / 2], arr[i - 1]);
-		}
-		else if (i % 3 == 0) {
-			arr[i] = 1 + min(arr[i / 3], arr[i - 1]);
-		}
-		else {
-			arr[i] = 1 + arr[i - 1];
-		}
-	}
-
-	cout << arr[n] << '\n';
-}
+//#include <iostream>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int arr[1'000'001];
+//
+//// top-down (recursive)
+//int recursive(int n) {
+//	if (n == 1) {
+//		return 0;
+//	}
+//	else if (n % 3 == 0 && n % 2 == 0) {
+//		arr[n] = 1 + min({ recursive(n / 3), recursive(n / 2), recursive(n - 1) });
+//		return arr[n];
+//	}
+//	else if (n % 3 == 0) {
+//		arr[n] = 1 + min(recursive(n / 3), recursive(n - 1));
+//		return arr[n];
+//	}
+//	else if (n % 2 == 0) {
+//		arr[n] = 1 + min(recursive(n / 2), recursive(n - 1));
+//		return arr[n];
+//	}
+//	else {
+//		arr[n] = 1 + recursive(n - 1);
+//		return arr[n];
+//	}
+//}
+//
+//int main() {
+//
+//	arr[1] = 0;
+//
+//	int n;
+//
+//	cin >> n;
+//
+//	// top-down (recursive)
+//	//cout << recursive(n) << '\n' << endl;
+//
+//	// bottom-up (for-loop)
+//	for (int i = 2; i <= n; ++i) {
+//	
+//		if (i % 2 == 0 && i % 3 == 0) {
+//			arr[i] = 1 + min({ arr[i / 2], arr[i / 3], arr[i - 1] });
+//		}
+//		else if (i % 2 == 0) {
+//			arr[i] = 1 + min(arr[i / 2], arr[i - 1]);
+//		}
+//		else if (i % 3 == 0) {
+//			arr[i] = 1 + min(arr[i / 3], arr[i - 1]);
+//		}
+//		else {
+//			arr[i] = 1 + arr[i - 1];
+//		}
+//	}
+//
+//	cout << arr[n] << '\n';
+//}
