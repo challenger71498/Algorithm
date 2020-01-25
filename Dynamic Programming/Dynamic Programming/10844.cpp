1bbde8598f7b2fefdbce5d31cd0bcb2bb48254f1
parang714 <challenger71498@gmail.com>
2020-01-25 15:08:30 +0900

    Add 11727

--- Dynamic Programming/Dynamic Programming/10844.cpp
+++ Dynamic Programming/Dynamic Programming/10844.cpp
@@ -1,35 +1,35 @@
-#include <iostream>
-#include <numeric>
-
-#define TRI 1'000'000'000
-
-using namespace std;
-
-long long arr[101][10];
-
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	
-	int n;
-	cin >> n;
-
-	for (int i = 1; i < 10; ++i) {
-		arr[1][i] = 1;
-	}
-
-	for (int i = 1; i < n; ++i) {
-		arr[i + 1][1] += arr[i][0];
-		arr[i + 1][1] %= TRI;
-		arr[i + 1][8] += arr[i][9] % TRI;
-		arr[i + 1][8] %= TRI;
-
-		for (int j = 1; j <= 8; ++j) {
-			arr[i + 1][j - 1] += arr[i][j];
-			arr[i + 1][j - 1] %= TRI;
-			arr[i + 1][j + 1] += arr[i][j];
-			arr[i + 1][j + 1] %= TRI;
-		}
-	}
-	cout << accumulate(arr[n], arr[n] + 10, 0ll) % TRI << '\n';
-}
+//#include <iostream>
+//#include <numeric>
+//
+//#define TRI 1'000'000'000
+//
+//using namespace std;
+//
+//long long arr[101][10];
+//
+//int main() {
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
+//	
+//	int n;
+//	cin >> n;
+//
+//	for (int i = 1; i < 10; ++i) {
+//		arr[1][i] = 1;
+//	}
+//
+//	for (int i = 1; i < n; ++i) {
+//		arr[i + 1][1] += arr[i][0];
+//		arr[i + 1][1] %= TRI;
+//		arr[i + 1][8] += arr[i][9] % TRI;
+//		arr[i + 1][8] %= TRI;
+//
+//		for (int j = 1; j <= 8; ++j) {
+//			arr[i + 1][j - 1] += arr[i][j];
+//			arr[i + 1][j - 1] %= TRI;
+//			arr[i + 1][j + 1] += arr[i][j];
+//			arr[i + 1][j + 1] %= TRI;
+//		}
+//	}
+//	cout << accumulate(arr[n], arr[n] + 10, 0ll) % TRI << '\n';
+//}
