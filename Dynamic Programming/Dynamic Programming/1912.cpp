52ff7531d0233b4e753bb3271ea656b6c60bc066
parang714 <challenger71498@gmail.com>
2020-01-25 15:03:19 +0900

    Add 10844

--- Dynamic Programming/Dynamic Programming/1912.cpp
+++ Dynamic Programming/Dynamic Programming/1912.cpp
@@ -1,30 +1,30 @@
-#include <iostream>
-#include <climits>
-#include <algorithm>
-
-using namespace std;
-
-int arr[100'001];
-int maximum = INT_MIN;
-
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	int n;
-	cin >> n;
-	for (int i = 1; i <= n; ++i) {
-		cin >> arr[i];
-	}
-
-	maximum = arr[1];
-
-	for (int i = 1; i < n; ++i) {
-		arr[i + 1] = max({ min(0, maximum), arr[i + 1], arr[i + 1] + arr[i] });
-		if (arr[i + 1] > maximum) {
-			maximum = arr[i + 1];
-		}
-	}
-
-	cout << maximum << '\n';
-}
+//#include <iostream>
+//#include <climits>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int arr[100'001];
+//int maximum = INT_MIN;
+//
+//int main() {
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
+//
+//	int n;
+//	cin >> n;
+//	for (int i = 1; i <= n; ++i) {
+//		cin >> arr[i];
+//	}
+//
+//	maximum = arr[1];
+//
+//	for (int i = 1; i < n; ++i) {
+//		arr[i + 1] = max({ min(0, maximum), arr[i + 1], arr[i + 1] + arr[i] });
+//		if (arr[i + 1] > maximum) {
+//			maximum = arr[i + 1];
+//		}
+//	}
+//
+//	cout << maximum << '\n';
+//}
