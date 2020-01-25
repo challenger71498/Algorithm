307645a5c63a85b9c2a36576de865038aed7c778
parang714 <challenger71498@gmail.com>
2020-01-25 12:01:49 +0900

    Add 2193

--- Algorithm/Algorithm/1149.cpp
+++ Algorithm/Algorithm/1149.cpp
@@ -1,33 +1,33 @@
-#include <iostream>
-#include <algorithm>
-
-using namespace std;
-
-int r[1001];
-int g[1001];
-int b[1001];
-
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	int n;
-	cin >> n;
-
-	for (int i = 0; i < n; ++i) {
-		int red, green, blue;
-
-		cin >> red >> green >> blue;
-		r[i] = red;
-		g[i] = green;
-		b[i] = blue;
-	}
-
-	for (int i = 1; i < n; ++i) {
-		r[i] += min(g[i - 1], b[i - 1]);
-		g[i] += min(r[i - 1], b[i - 1]);
-		b[i] += min(r[i - 1], g[i - 1]);
-	}
-
-	cout << min({ r[n - 1], g[n - 1], b[n - 1] }) << '\n';
-}
+//#include <iostream>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int r[1001];
+//int g[1001];
+//int b[1001];
+//
+//int main() {
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
+//
+//	int n;
+//	cin >> n;
+//
+//	for (int i = 0; i < n; ++i) {
+//		int red, green, blue;
+//
+//		cin >> red >> green >> blue;
+//		r[i] = red;
+//		g[i] = green;
+//		b[i] = blue;
+//	}
+//
+//	for (int i = 1; i < n; ++i) {
+//		r[i] += min(g[i - 1], b[i - 1]);
+//		g[i] += min(r[i - 1], b[i - 1]);
+//		b[i] += min(r[i - 1], g[i - 1]);
+//	}
+//
+//	cout << min({ r[n - 1], g[n - 1], b[n - 1] }) << '\n';
+//}
