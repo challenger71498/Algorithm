2ccfd0192b6e474610a683b63fb9f1cbcd271bf7
parang714 <challenger71498@gmail.com>
2020-01-26 11:50:29 +0900

    Add 11053

--- Dynamic Programming/Dynamic Programming/11727.cpp
+++ Dynamic Programming/Dynamic Programming/11727.cpp
@@ -1,22 +1,22 @@
-#include <iostream>
-
-using namespace std;
-
-int arr[1001];
-
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	int n;
-	cin >> n;
-
-	arr[1] = 1;
-	arr[2] = 3;
-
-	for (int i = 3; i <= n; ++i) {
-		arr[i] = (arr[i - 1] + arr[i - 2] * 2) % 10'007;
-	}
-
-	cout << arr[n] << '\n';
-}
+//#include <iostream>
+//
+//using namespace std;
+//
+//int arr[1001];
+//
+//int main() {
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
+//
+//	int n;
+//	cin >> n;
+//
+//	arr[1] = 1;
+//	arr[2] = 3;
+//
+//	for (int i = 3; i <= n; ++i) {
+//		arr[i] = (arr[i - 1] + arr[i - 2] * 2) % 10'007;
+//	}
+//
+//	cout << arr[n] << '\n';
+//}
