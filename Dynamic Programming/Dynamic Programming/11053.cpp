f2265fcee0c41355d96e6a728f28d8646e7f1161
parang714 <challenger71498@gmail.com>
2020-01-26 12:23:41 +0900

    Add 11052

--- Dynamic Programming/Dynamic Programming/11053.cpp
+++ Dynamic Programming/Dynamic Programming/11053.cpp
@@ -1,35 +1,35 @@
-#include <iostream>
-#include <algorithm>
-#include <climits>
-
-using namespace std;
-
-int arr[1001];
-int dp[1001];
-
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	int n;
-	cin >> n;
-	for (int i = 1; i <= n; ++i) {
-		cin >> arr[i];
-	}
-
-	for (int i = 1; i <= n; ++i) {
-		if (dp[i] == 0) {
-			dp[i] = 1;
-		}
-
-		for (int j = 1; j <= i; ++j) {
-			if (arr[j] < arr[i]) {
-				if (dp[i] < dp[j] + 1) {
-					dp[i] = dp[j] + 1;
-				}
-			}
-		}
-	}
-
-	cout << *max_element(dp + 1, dp + n + 1) << '\n';
-}
+//#include <iostream>
+//#include <algorithm>
+//#include <climits>
+//
+//using namespace std;
+//
+//int arr[1001];
+//int dp[1001];
+//
+//int main() {
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
+//
+//	int n;
+//	cin >> n;
+//	for (int i = 1; i <= n; ++i) {
+//		cin >> arr[i];
+//	}
+//
+//	for (int i = 1; i <= n; ++i) {
+//		if (dp[i] == 0) {
+//			dp[i] = 1;
+//		}
+//
+//		for (int j = 1; j <= i; ++j) {
+//			if (arr[j] < arr[i]) {
+//				if (dp[i] < dp[j] + 1) {
+//					dp[i] = dp[j] + 1;
+//				}
+//			}
+//		}
+//	}
+//
+//	cout << *max_element(dp + 1, dp + n + 1) << '\n';
+//}
