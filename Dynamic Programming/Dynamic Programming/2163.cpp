ee0082940343901f0826856b56f8049071d0a91c
parang714 <challenger71498@gmail.com>
2020-01-26 14:51:02 +0900

    Add 9465

--- Dynamic Programming/Dynamic Programming/2163.cpp
+++ Dynamic Programming/Dynamic Programming/2163.cpp
@@ -1,13 +1,13 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	int n, m;
-	cin >> n >> m;
-
-	cout << n * m - 1 << '\n';
-}
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
+//
+//	int n, m;
+//	cin >> n >> m;
+//
+//	cout << n * m - 1 << '\n';
+//}
---
+++ Dynamic Programming/Dynamic Programming/9465.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int arr[100'001][2];
+int dp[100'001][3];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; ++i) {
+		int n;
+		cin >> n;
+		for (int j = 1; j <= n; ++j) {
+			cin >> arr[j][0];
+		}
+		for (int j = 1; j <= n; ++j) {
+			cin >> arr[j][1];
+		}
+
+		dp[1][0] = arr[1][0];
+		dp[1][1] = arr[1][1];
+		dp[1][2] = 0;
+
+		for (int j = 2; j <= n; ++j) {
+			dp[j][0] = max({ dp[j - 1][1] + arr[j][0], dp[j - 1][2] + arr[j][0], arr[j][0] });
+			dp[j][1] = max({ dp[j - 1][0] + arr[j][1], dp[j - 1][2] + arr[j][1], arr[j][1] });
+			dp[j][2] = max({ dp[j - 1][0], dp[j - 1][1], dp[j - 1][2] });
+		}
+
+		cout << *max_element(dp[n], dp[n] + 2) << '\n';
+	}
+}
