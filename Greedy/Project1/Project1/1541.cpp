05212829e92ffefc5959a5da079587c1d198dbdc
challenger71498 <challenger71498@gmail.com>
2020-01-31 20:13:24 +0900

    Add 1541

---
+++ Greedy/Project1/Project1/1541.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+char str[51];
+
+int nums[51];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> str;
+
+	string s;
+	int counter = 0;
+	for (int i = 0; str[i] != '\0'; ++i) {
+		// str[i] is a number
+		if ('0' <= str[i] && str[i] <= '9') {
+			s += str[i];
+		} // not a number -> terminate
+		else {
+			nums[counter] += stoi(s);
+			s = "";
+			if (str[i] == '-') {
+				counter++;
+			}
+		}
+	}
+
+	if (s != "") {
+		nums[counter] += stoi(s);
+	}
+
+	int total = nums[0];
+	for (int i = 1; nums[i] != 0; ++i) {
+		total -= nums[i];
+	}
+
+	cout << total << '\n';
+}
